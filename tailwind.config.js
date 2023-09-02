e02e2581106cdd3499d68d6a0a1be79e2666068e
caleb <calebadeoye639@gmail.com>
2023-09-02 02:25:11 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans'], // Define your font here
+      },
+    },
+  },
+  
+  plugins: [],
+}
+
